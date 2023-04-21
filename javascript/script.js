8faebdb37b797eca789bcb2c86009d96928c1873
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-04-21 18:02:24 +0200

    add this .toggle to cell

--- javascript/script.js
+++ javascript/script.js
@@ -3,7 +3,7 @@ creare una griglia 10 x 10
 generare i quadratini della presenti all'interno della griglia
 */
 
-const eleGrid = document.querySelector('.grid');
+let eleGrid = document.querySelector('.grid');
 
 let mainTitle = document.querySelector('.hidden');
 
@@ -20,12 +20,9 @@ eleButton.addEventListener('click', function () {
 
     for (let i = 0; i < listCells.length; i++) {
         const cell = listCells[i];
-        cell.addEventListener('click', 
-        function colorCell() {
-            console.log(this);
+        cell.addEventListener('click', function () {
             this.classList.toggle('clicked');
-        }
-        ) 
+        }) 
      }
      
      for (let i = 1; i <= 100; i++) {
@@ -33,33 +30,18 @@ eleButton.addEventListener('click', function () {
      }
 })
 
-/*
-
-for (let i = 0; i < listCells.length; i++) {
-   const cell = listCells[i];
-   cell.addEventListener('click', 
-   function colorCell() {
-       console.log(this);
-       this.classList.toggle('clicked');
-   }
-   ) 
-}
-
-for (let i = 1; i <= 100; i++) {
-    console.log(i);
-}
-
-*/
-
-
 // functions
 
 function createGrid(numCells) {
+    eleGrid.innerHTML = '';
     for (let i = 0; i < numCells; i++) {
 
         eleGrid.innerHTML += `<div class="cell">${i + 1}<div>`
     }
 }
+
+
+
 
 
    
