a9dd52929f1dc0acae29f40889f8bdd973d5431d
Giovanni-Palmitessa <giovicchio@gmail.com>
2023-04-24 15:47:16 +0200

    change value to options

--- javascript/script.js
+++ javascript/script.js
@@ -11,12 +11,12 @@ let eleButton = document.querySelector('#play');
 
 mainTitle.innerHTML = 'Seleziona il livello di difficoltà e genera la griglia';
 
-let selectDifficulty = document.querySelector('#difficulty').value;
+/* DA QUI CODICE GIUSTO
 
 eleButton.addEventListener('click', function () {
     mainTitle.innerHTML = '';
 
-    createGrid (81);
+    createGrid (100);
 
     const listCells = document.querySelectorAll('.cell');
 
@@ -38,10 +38,45 @@ function createGrid(numCells) {
     eleGrid.innerHTML = '';
     for (let i = 0; i < numCells; i++) {
 
-        eleGrid.innerHTML += `<div class="cell-md">${i + 1}<div>`
+        eleGrid.innerHTML += `<div class="cell">${i + 1}<div>`
     }
 }
 
+*/
+
+
+eleButton.addEventListener('click', function () {
+    const eleDifficulty = document.getElementById("difficulty");
+    let value = eleDifficulty.options[eleDifficulty.selectedIndex].value;
+    console.log(value);
+    
+    mainTitle.innerHTML = '';
+
+    createGrid (100);
+
+    const listCells = document.querySelectorAll('.cell');
+
+    for (let i = 0; i < listCells.length; i++) {
+        const cell = listCells[i];
+        cell.addEventListener('click', function () {
+            this.classList.toggle('clicked');
+        }) 
+     }
+     
+     for (let i = 1; i <= 100; i++) {
+        console.log(i);
+     }
+})
+
+// functions
+
+function createGrid(numCells) {
+    eleGrid.innerHTML = '';
+    for (let i = 0; i < numCells; i++) {
+
+        eleGrid.innerHTML += `<div class="cell">${i + 1}<div>`
+    }
+}
 
 
 
